4328da0ef9b7a0b3033158973e3547cc21621c48
Miles Aikens <miles@milesaikens.com>
2017-01-17 17:08:49 -0500

    Added XBox controller static ints for easy access

---
+++ src/main/java/org/usfirst/frc/team3322/XBox.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team3322;
+
+/**
+ * Created by sneki on 1/17/2017.
+ */
+public class XBox {
+    public static final int ABUTTON = 1;
+    public static final int BBUTTON = 2;
+    public static final int XBUTTON = 3;
+    public static final int YBUTTON = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int BACK = 7;
+    public static final int START = 8;
+    public static final int LSTICK_PRESS = 9;
+    public static final int RSTICK_PRESS = 10;
+
+    public static final int DPADVERT = 7; //these have prooven to be unreliable
+    public static final int DPADHORIZ = 6;
+}
