062e06b5dfaa859905b715a61607257477b5157e
Andriy Massimilla <andriymassimilla@gmail.com>
2017-01-17 17:41:04 -0500

    Add OI (Operator Interface) class

--- src/main/java/org/usfirst/frc/team3322/Robot.java
+++ src/main/java/org/usfirst/frc/team3322/Robot.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team3322;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SerialPort;
 
 /**
@@ -11,20 +10,16 @@ import edu.wpi.first.wpilibj.SerialPort;
  * Main Robot class for 3322's 2017 SteamWorks robot
  */
 public class Robot extends IterativeRobot {
-    //define our global variables
-    private Joystick driver, tech;
-    private AHRS navx;
+    // Define our global variables
     Drivetrain drivetrain;
     Compressor compressor;
+	private AHRS navx;
 
-    @Override
+	@Override
     public void robotInit() {
-        //init our compressor as PCM number 1
+        // Init our compressor as PCM number 1
         compressor = new Compressor(1);
-        //init the driver joystick as number 0, and tech as number 1
-        driver = new Joystick(0);
-        tech = new Joystick(1);
-        //NavX gyroscope init
+        // NavX gyroscope init
         navx = new AHRS(SerialPort.Port.kUSB);
         drivetrain = new Drivetrain();
         drivetrain.init(true,false);
