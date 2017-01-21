685a7825004407e54f5f068a1b084e8833fe5243
FRC <danieljnchen@gmail.com>
2017-01-20 21:04:08 -0500

    Rename GearCollection to Gear

--- src/main/java/org/usfirst/frc/team3322/Gear.java
+++ src/main/java/org/usfirst/frc/team3322/Gear.java
@@ -2,20 +2,24 @@ package org.usfirst.frc.team3322;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-public class GearCollection {
+public class Gear {
 	DoubleSolenoid gearSolenoid; // Main solenoid to control linear actuator
 	DoubleSolenoid pitchSolenoid; // Controls pitch of collection assembly
 
-	void init() {
+	public Gear() {
 		gearSolenoid = new DoubleSolenoid(0, 1);
 		pitchSolenoid = new DoubleSolenoid(2, 3);
 	}
 
-	public static void toggleSolenoid(DoubleSolenoid solenoid) {
+	void toggleSolenoid(DoubleSolenoid solenoid) {
 		if (solenoid.get() == DoubleSolenoid.Value.kReverse) {
 			solenoid.set(DoubleSolenoid.Value.kForward);
 		} else {
 			solenoid.set(DoubleSolenoid.Value.kReverse);
 		}
 	}
+
+	public void extendHolder() {
+	    toggleSolenoid(gearSolenoid);
+    }
 }
