e2f9eabe866340d326dcbac14fc588467e126293
ShivamGPatel <shivamgpatel2001@gmail.com>
2017-01-26 20:32:37 -0500

    Added new Dashboard class

---
+++ src/main/java/org/usfirst/frc/team3322/Dashboard.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team3322;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+/**
+ * Created by Shivam Patel on 1/26/2017.
+ */
+public class SmartDashboard {
+    SmartDashboard smartDashboard;
+    public SmartDashboardJava() {
+        smartDashboard = new SmartDashboard();
+    }
+    public void printInfo() {
+        
+        
+    }
+    }   
--- src/main/java/org/usfirst/frc/team3322/SmartDashboard.java
+++
@@ -1,7 +0,0 @@
-package org.usfirst.frc.team3322;
-
-/**
- * Created by Shivam Patel on 1/26/2017.
- */
-public class SmartDashboard {
-}
