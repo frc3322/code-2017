5031aead5946ad0fcfe3e8911f8bd55a69be4670
Andriy Massimilla <andriymassimilla@gmail.com>
2017-01-27 23:29:12 -0500

    Add RobotMap

---
+++ src/main/java/org/usfirst/frc/team3322/RobotMap.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3322;
+
+
+public class RobotMap {
+    // Assign ids to integers for easier electrical rewiring
+    public static final int
+            // Drivetrain talons
+            driveLeft_1 = 20,
+            driveLeft_2 = 21,
+            driveLeft_3 = 3,
+
+            driveRight_1 = 22,
+            driveRight_2 = 23,
+            driveRight_3 = 6,
+
+            // Climber talons
+            climbTalon_1 = 77,
+            climbTalon_2 = 78;
+}
